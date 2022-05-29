c1cec1ba9b27ee8dad2f0ec2cb3f2029e9d7b92f
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2022-05-29 23:29:41 +0200

    last fix

--- components/layout/sections/General/Carousel.jsx
+++ components/layout/sections/General/Carousel.jsx
@@ -32,15 +32,15 @@ export default function App() {
 
   const slides = [
     {
-      img: "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "/assets/images/brand/slide3.jpg",
       label: "The arctic experience",
     },
     {
-      img: "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
+      img: "/assets/images/brand/slide2.jpg",
       label: "See for yourself",
     },
     {
-      img: "https://images.pexels.com/photos/1142950/pexels-photo-1142950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+      img: "/assets/images/brand/slide1.jpg",
       label: "Discover more",
     },
   ];
--- components/layout/sections/General/Gallery.jsx
+++ components/layout/sections/General/Gallery.jsx
@@ -43,25 +43,20 @@ export default function Gallery() {
 
   const slides = [
     {
-      img: "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      label: "First Slide",
+      img: "/assets/images/brand/slide3.jpg",
+      label: "The arctic experience",
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
     {
-      img: "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-      label: "Third Slide",
+      img: "/assets/images/brand/slide2.jpg",
+      label: "See for yourself",
       description:
         "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
     },
     {
-      img: "https://images.pexels.com/photos/1142950/pexels-photo-1142950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      label: "Fourth Slide",
+      img: "/assets/images/brand/slide1.jpg",
+      label: "Discover more",
       description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
-    },
-    {
-      img: "https://images.pexels.com/photos/3124111/pexels-photo-3124111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      label: "Fifth Slide",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     },
   ];
 
