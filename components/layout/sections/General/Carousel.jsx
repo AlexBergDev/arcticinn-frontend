ab45a188a597397dbdcd1ce774bd83d76ad48d43
AlexBergDev <68918064+AlexBergDev@users.noreply.github.com>
2022-05-29 21:47:14 +0200

    Bug fix

--- components/layout/sections/General/Carousel.jsx
+++ components/layout/sections/General/Carousel.jsx
@@ -33,24 +33,15 @@ export default function App() {
   const slides = [
     {
       img: "https://images.pexels.com/photos/2714581/pexels-photo-2714581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      label: "Straumsnes",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      label: "The arctic experience",
     },
     {
       img: "https://images.pexels.com/photos/2878019/pexels-photo-2878019.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
-      label: "Third Slide",
-      description:
-        "Praesent commodo cursus magna, vel scelerisque nisl consectetur.",
+      label: "See for yourself",
     },
     {
       img: "https://images.pexels.com/photos/1142950/pexels-photo-1142950.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      label: "Fourth Slide",
-      description: "Nulla vitae elit libero, a pharetra augue mollis interdum.",
-    },
-    {
-      img: "https://images.pexels.com/photos/3124111/pexels-photo-3124111.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-      label: "Fifth Slide",
-      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      label: "Discover more",
     },
   ];
 
